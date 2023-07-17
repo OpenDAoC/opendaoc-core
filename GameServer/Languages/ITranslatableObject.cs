12b9bed019b0dd170006dc6bbbb2fe3ea4007ae9
Deftness <115406842+deftness02@users.noreply.github.com>
2023-07-17 11:52:36 -0600

    Refactored Languages directory

---
+++ GameServer/Languages/ITranslatableObject.cs
@@ -0,0 +1,11 @@
+﻿using DOL.Database;
+
+namespace DOL.Language
+{
+    public interface ITranslatableObject
+    {
+        string TranslationId { get; set; }
+
+        LanguageDataObject.ETranslationIdentifier TranslationIdentifier { get; }
+    }
+}
